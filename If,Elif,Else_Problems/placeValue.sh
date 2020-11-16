855505705002c86c8378cd649c5f0197f2d2f5a7
unknown <miyashaheen1995@gmail.com>
2020-11-17 04:56:07 +0530

    Added : If elif & else practice problems

---
+++ If,Elif,Else_Problems/placeValue.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+read -p "Enter number:-" num
+if [ $num -eq 1 ]
+then
+	echo "Unit"
+elif [ $num -eq 10 ]
+then
+	echo "Ten"
+elif [ $num -eq 100 ]
+then
+	echo "Hundred"
+elif [ $num -eq 1000 ]
+then
+	echo "Thousand"
+else
+	echo "Invalid Input"
+fi
